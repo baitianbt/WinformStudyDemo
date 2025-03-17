cf334a4de4a966f5315b2bf9e7014482423e6793
dong <1749492810@qq.com>
2025-03-17 10:24:48 +0800

    communitytookit

---
+++ winform基于Communitytoolkit.MVVM登录界面DEMO/Form1.cs
@@ -0,0 +1,10 @@
+namespace winform基于Communitytoolkit.MVVM登录界面DEMO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
